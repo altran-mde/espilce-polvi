8f92e8d026fb6e310dc9e646d6a4c5071e9ee806
Niko Stotz <niko.stotz@altran.com>
2019-09-05 16:07:34 +0200

    replaced all names commons->polvi

---
+++ component/emf.generator.xtext/org.espilce.polvi.emf.generator.xtext.dependency.pckg.test/src-test/org/espilce/polvi/emf/generator/xtext/dependency/pckg/test/TestEmfGeneratorXtextDependency.java
@@ -0,0 +1,49 @@
+package org.espilce.polvi.emf.generator.xtext.dependency.pckg.test;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.espilce.polvi.emf.generator.xtext.PolviDelegateGenerator;
+import org.espilce.polvi.emf.generator.xtext.XtextDelegateCancelIndicator;
+import org.espilce.polvi.emf.generator.xtext.XtextDelegateFileSystemAccess;
+import org.espilce.polvi.emf.generator.xtext.XtextDelegateGeneratorContext;
+import org.espilce.polvi.emf.generator.xtext.ext.InMemoryFileSystemAccess4;
+import org.espilce.polvi.emf.generator.xtext.ext.JavaIoFileSystemAccess4;
+import org.espilce.polvi.emf.generator.xtext.ext.URIBasedFileSystemAccess4;
+import org.junit.Test;
+
+public class TestEmfGeneratorXtextDependency {
+	@Test
+	public void polviDelegateGenerator() throws Exception {
+		assertNotNull(new PolviDelegateGenerator(null));
+	}
+	
+	@Test
+	public void xtextDelegateCancelIndicator() throws Exception {
+		assertNotNull(new XtextDelegateCancelIndicator(null));
+	}
+	
+	@Test
+	public void xtextDelegateFileSystemAccess() throws Exception {
+		assertNotNull(new XtextDelegateFileSystemAccess(null));
+	}
+	
+	@Test
+	public void xtextDelegateGeneratorContext() throws Exception {
+		assertNotNull(new XtextDelegateGeneratorContext(null));
+	}
+	
+	@Test
+	public void inMemoryFileSystemAccess4() throws Exception {
+		assertNotNull(new InMemoryFileSystemAccess4());
+	}
+	
+	@Test
+	public void javaIoFileSystemAccess4() throws Exception {
+		assertNotNull(new JavaIoFileSystemAccess4());
+	}
+	
+	@Test
+	public void uRIBasedFileSystemAccess4() throws Exception {
+		assertNotNull(new URIBasedFileSystemAccess4());
+	}
+}
---
+++ component/generator/org.espilce.polvi.generator.dependency.bundle.test/src-test/org/espilce/polvi/generator/dependency/bundle/test/TestGeneratorDependency.java
@@ -0,0 +1,40 @@
+package org.espilce.polvi.generator.dependency.bundle.test;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import org.espilce.polvi.generator.AbstractFileSystemAccess;
+import org.espilce.polvi.generator.outputconfig.OutputConfiguration;
+import org.espilce.polvi.generator.outputconfig.SourceMapping;
+import org.junit.Test;
+
+public class TestGeneratorDependency {
+	@Test
+	public void abstractFileSystemAccess() throws Exception {
+		final AtomicBoolean b = new AtomicBoolean(false);
+		new AbstractFileSystemAccess() {
+			
+			@Override
+			public void generateFile(
+					final String fileName, final String outputConfigurationName,
+					final CharSequence contents
+			) {
+				b.set(true);
+			}
+		}.generateFile("a", "b", "c");
+		assertTrue(b.get());
+	}
+	
+	@Test
+	public void outputConfiguration() throws Exception {
+		assertNotNull(new OutputConfiguration("hello").getName());
+		
+	}
+	
+	@Test
+	public void sourceMapping() throws Exception {
+		assertNotNull(new SourceMapping("asdf").getSourceFolder());
+	}
+}
